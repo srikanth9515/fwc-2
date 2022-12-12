373cd8417346b5efe904bbde1973b51681e7242a
satheeshsimha <109905742+satheeshsimha@users.noreply.github.com>
2022-12-12 13:23:15 +0530

    Add files via upload

--- assembly/sevenseg/codes/sevenseg.asm
+++ assembly/sevenseg/codes/sevenseg.asm
@@ -2,7 +2,7 @@
 ;displaying number on
 ;seven segment display
 
-.include "/home/gadepall/m328Pdef.inc"
+.include "/home/satheeshsk/m328Pdef.inc"
 
   
 ;Configuring pins 2-7 (PD2-PD7) of Arduino
@@ -15,7 +15,7 @@
   out DDRB,r16
 ;Writing the number 2 on the 
 ;seven segment display
-  ldi r17,0b10010000
+  ldi r17,0b00000000
   out PortD,r17
   
   ldi r17,0b00000000
